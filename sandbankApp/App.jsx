c9da93dc952822b1417f3b7ff66c4fe76be91a6b
Kae <kaehehehe@gmail.com>
2022-05-01 11:01:27 +0900

    :sparkles: Bottom Tab Navigation 구현

--- sandbankApp/App.jsx
+++ sandbankApp/App.jsx
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+import HomeScreen from './screens/Home';
+import InvestmentScreen from './screens/Investment';
+import InfoScreen from './screens/Information';
+import SettingScreen from './screens/Setting';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={HomeScreen}
+        screenOptions={() => ({
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          tabBarLabelStyle: { marginBottom: 10, fontSize: 14 },
+          tabBarStyle: { height: 70 },
+        })}
+      >
+        <Tab.Screen
+          name="홈"
+          component={HomeScreen}
+          options={{
+            title: '홈',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="투자"
+          component={InvestmentScreen}
+          options={{
+            title: '투자',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="trending-up-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="인포"
+          component={InfoScreen}
+          options={{
+            title: '인포',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="bar-chart-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="설정"
+          component={SettingScreen}
+          options={{
+            title: '설정',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="settings-outline" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ sandbankApp/screens/Investment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Investment = () => {
+  return (
+    <View>
+      <Text>Investment Screen</Text>
+    </View>
+  );
+};
+
+export default Investment;
