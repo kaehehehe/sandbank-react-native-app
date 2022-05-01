fe55b44caefdfadca9f1e6bc3a630d5d2cd56294
Kae <kaehehehe@gmail.com>
2022-05-01 12:54:45 +0900

    :sparkles: Top Tab Navigation 구현

--- sandbankApp/screens/Information.jsx
+++ sandbankApp/screens/Information.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+import BJobScreen from '../Information/BJob';
+import YoutubeScreen from '../Information/Youtube';
+import InsightScreen from '../Information/Insight';
+import QuizScreen from '../Information/Quiz';
+import MarketScreen from '../Information/Market';
+
+const Tab = createMaterialTopTabNavigator();
 
 const Information = () => {
   return (
-    <View>
-      <Text>Info Screen</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="알쓸B잡" component={BJobScreen} />
+      <Tab.Screen name="유튜브" component={YoutubeScreen} />
+      <Tab.Screen name="인사이트" component={InsightScreen} />
+      <Tab.Screen name="퀴즈" component={QuizScreen} />
+      <Tab.Screen name="마켓" component={MarketScreen} />
+    </Tab.Navigator>
   );
 };
 
